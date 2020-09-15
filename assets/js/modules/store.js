6781ea8dcd8da2440cc0c3cbc886f7cabeeb09b2
shervin dadashzade <shervin.dadashzade7988@gmail.com>
2020-09-16 00:56:43 +0430

    store module added

---
+++ assets/js/modules/store.js
@@ -0,0 +1,11 @@
+import Todo from './todo-class.js';
+
+let store = {
+    todos: [
+        new Todo('مسواک زدن','هر شب راس ساعت 10 مسواک میزنی','--var(primary)',false),
+        new Todo('خرید گوشت برای خانه','تو راه برگشت از سر کار نیم کیلو گوشت از غیاث آباد برای آبگوشت بخرم','--var(primary)',false),
+        new Todo('کتاب پدر فقیر پدر پول دار','کتاب رو از فردوسی بخر','--var(primary)',false),
+    ],
+};
+
+export default store;
